53ba431965372d12e9d241da94fb38c17fcad995
Rafael Fernandes <rafaelsf013@gmail.com>
2025-01-05 08:15:41 -0300

    feat: Add routes.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-use actix_web::{web, App, HttpServer, Responder};
+mod models;
+mod handlers;
+
+use actix_web::{web, App, HttpServer};
 
 const ADDRESS_SERVER: &str = "127.0.0.1:8080";
-
-async fn hello_world() -> impl Responder{
-    "Hello World!"
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
     HttpServer::new(|| {
             App::new()
-                .route("/", web::get().to(hello_world))
+                .service(
+                    web::scope("/api")
+                        .route("/champions",
+                            web::post().to(handlers::create_champion)),
+                )
     })
     .bind(ADDRESS_SERVER)?
     .run()
